2b485dee999fb068f30b6f0e9fde560adba7745b
gauravpatil123 <gaurav.patil123@gmail.com>
2023-04-05 12:51:28 -0400

    Added arrow prototypes in landing page

--- landingPage.js
+++ landingPage.js
@@ -1,6 +1,32 @@
-$("#enter-button").hover(function() {
-    $(this).html("Enter")
+const $icon = document.querySelector('.icon');
+const $arrow = document.querySelector('.arrow');
+
+$arrow.animate([
+    {top: '0'},
+    {top: '6px'},
+    {top: '0'}
+  ],{
+    duration: 1000,
+    iterations: Infinity
+  });
+
+$("#enter-button-wrapper").hover(function() {
+    $("#enter-button-link").html("Enter");
+    $(".icon").css("display", "none");
+    $(".arrow-2").css("display", "none");
+    $(".arrow-3").css("display", "none");
 }, function(){
-    $(this).html("")
+    $("#enter-button-link").html("")
+    $(".icon").css("display", "block");
+    $(".arrow-2").css("display", "block");
+    $(".arrow-3").css("display", "block");
+    $arrow.animate([
+        {top: '0'},
+        {top: '10px'},
+        {top: '0'}
+      ],{
+        duration: 1000,
+        iterations: Infinity
+      });
 })
 
