379ed6632d0000355520d150a718f4512f455460
gauravpatil123 <gaurav.patil123@gmail.com>
2023-04-09 22:35:02 -0400

    initialized project thumbnails hover animation & titles template

--- homepage.js
+++ homepage.js
@@ -1,5 +1,17 @@
 // import { setCss } from "./landingPage";
 
+//global variable
+var project_titles = {
+    "project-1" : "Project 1 title",
+    "project-2" : "Project 2 title",
+    "project-3" : "Project 3 title",
+    "project-4" : "Project 4 title",
+    "project-5" : "Project 5 title",
+    "project-6" : "Project 6 title",
+    "project-7" : "Project 7 title",
+    "project-8" : "Project 8 title"
+}
+
 function setCss(querySelector, property, value) {
 
     $(querySelector).css(property, value);
@@ -12,40 +24,40 @@ async function main() {
     $("#filter-data").on("click", function(){
 
         setCss(".portfolio-piece-box", "display", "none");
-        setCss(".data", "display", "block");
+        setCss(".data", "display", "flex");
 
     });
 
     $("#filter-design").on("click", function(){
 
         setCss(".portfolio-piece-box", "display", "none");
-        setCss(".design", "display", "block");
+        setCss(".design", "display", "flex");
 
     });
 
     $("#filter-document").on("click", function(){
 
         setCss(".portfolio-piece-box", "display", "none");
-        setCss(".document", "display", "block");
+        setCss(".document", "display", "flex");
 
     });
 
     $("#filter-all").on("click", function(){
 
-        setCss(".portfolio-piece-box", "display", "block");
+        setCss(".portfolio-piece-box", "display", "flex");
 
     });
 
-    //make mouse hover function to delete html for id and 
-    // add opacity and other features to .inner class div to 
-    // make it pop out in thumbnail
     $(".portfolio-piece-box").hover(function(){
 
-        console.log("mouse enter " + this.id);
+        var selector = "#" + this.id + " .inner .project-thumbnail-title";
+        var title_text = project_titles[this.id];
+        $(selector).html(title_text);
 
     }, function(){
 
-        console.log("mouse exit");
+        var selector = "#" + this.id + " .inner .project-thumbnail-title";
+        $(selector).html("");
 
     })
 
