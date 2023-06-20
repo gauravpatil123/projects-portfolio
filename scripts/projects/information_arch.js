2f9e780ed6b0a71d4b16eddcb3259bafd0cf164c
gauravpatil123 <gaurav.patil123@gmail.com>
2023-06-20 15:09:20 -0400

    updated about in information arch

--- scripts/projects/information_arch.js
+++ scripts/projects/information_arch.js
@@ -10,6 +10,15 @@ var my_role_text2 = "<b>Design:</b> Information Architecture, Paper Sketches, Pa
 
 var fig2_caption = "Fig #2: <i>Project timeline showing various aspects of the project during its 15 weeks</i>";
 
+var about_text1 = "Founded in 1976, the New York Transit Museum is dedicated to telling & preserving the stories of mass transportation. Keeping this goal in mind, the museum wants its visitors to have easy access to all its programs and offerings from their website. Hence they wanted designers to revise their sitemap & information architecture to achieve this goal.";
+var fig3_4_caption = "Fig #3 & #4: <i>Screenshots of current NYTM webpages | Homepage & Tickets Page</i>";
+var about_text2 = "Further, since the New York Transit Museum is a self-supporting division of the Metropolitan Transportation Authority, they also wanted designers to improve their ticket purchase process on the website and make it smooth & intuitive for their visitors.";
+var about_text3 = "All these groups are potential visitors of the NYTM since the museum has educational, historic & informative offerings that have appeal for all these groups. Thus all these groups are essential for our research and are our target users."; 
+var fig5_6_7_caption = "Fig #5, #6 & #7: <i>Icons Respectively Representing Educators, Students & Museum Enthusiasts</i>";
+var about_text4 = "After the kick-off meeting with the client, we decided on the following goals for project:";
+var about_text5 = "1. Make the process of purchasing tickets through their website easy & intuitive.";
+var about_text6 = "2. Revise the Information architecture of the website to make its programs & offerings easily & intuitively accessible to their users.";
+
 function main() {
 
     $("#fig1-caption").html(fig1_caption);
@@ -29,6 +38,22 @@ function main() {
     $("#my-role-text2").html(my_role_text2);
 
     $("#fig2-caption").html(fig2_caption);
+
+    $("#about-text1").html(about_text1);
+
+    $("#fig3-4-caption").html(fig3_4_caption);
+
+    $("#about-text2").html(about_text2);
+
+    $("#about-text3").html(about_text3);
+
+    $("#fig5-6-7-caption").html(fig5_6_7_caption);
+
+    $("#about-text4").html(about_text4);
+
+    $("#about-text5").html(about_text5);
+
+    $("#about-text6").html(about_text6);
 
 }
 
