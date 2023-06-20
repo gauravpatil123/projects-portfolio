17818e70ebbd67ff0adefdec652bc2d5fe7b405a
gauravpatil123 <gaurav.patil123@gmail.com>
2023-06-20 14:24:56 -0400

    updated intodustion & project timeline in information arch

--- scripts/projects/information_arch.js
+++ scripts/projects/information_arch.js
@@ -1,4 +1,34 @@
+
+var fig1_caption = "Fig #1: <i>Screenshot of the Redesigned Mid-Fidelity NYTM Desktop Homepage</i>";
+var introduction_text = "The New York Transit Museum displays historical artifacts of the New York City Subway, bus & commuter rail systems in the greater New York City metropolitan region. This project attempts to improve the user experience of NYTM’s website based on insights from user interviews, market research and through the use of personas, user stories, revamped information architecture, wireframes & prototypes.";
+var client_text = "New York Transit Museum";
+var duration_text = "15 Weeks";
+var team_text = "Nouran Sedaghat, Gaurav Patil, Julia Maloof";
+var all_tools_text = "Miro, Google Workspace, Optimal Workshop, Sketch, Invision, Figma";
+var my_role_text1 = "<b>Research & Analysis:</b> Screener forms, Questionnaires, Interview Protocol, Interviews, Affinity Mapping, Persona, Competitive Analysis, Card Sorting, Usability Testing, Tree Testing.";
+var my_role_text2 = "<b>Design:</b> Information Architecture, Paper Sketches, Paper Prototypes, Medium-Fidelity Prototypes of “School Field Trip” pages.";
+
+var fig2_caption = "Fig #2: <i>Project timeline showing various aspects of the project during its 15 weeks</i>";
+
 function main() {
+
+    $("#fig1-caption").html(fig1_caption);
+
+    $("#introduction-text").html(introduction_text);
+
+    $("#client-text").html(client_text);
+
+    $("#duration-text").html(duration_text);
+
+    $("#team-text").html(team_text);
+
+    $("#all-tools-text").html(all_tools_text);
+
+    $("#my-role-text1").html(my_role_text1);
+
+    $("#my-role-text2").html(my_role_text2);
+
+    $("#fig2-caption").html(fig2_caption);
 
 }
 
