b3de499c71f9eb9ca623ef652a894409876ae1a6
gauravpatil123 <gaurav.patil123@gmail.com>
2023-07-14 18:23:33 -0400

    updated panel for digital product design

--- scripts/projects/digital_product_design_panel.js
+++ scripts/projects/digital_product_design_panel.js
@@ -2,7 +2,7 @@ import {setCss, append_tag, append_link, get_link_url} from "../utils.js";
 
 var project_thumbnail_url = "../../assets/thumbnails/nyc-ddc-product-design-sp23-3.png";
 var title = "Communicating Subsurface Infrastructure for NYC DDC";
-var skills = ["Wireframing", "Prototype Design", "User Testing"];
+var skills = ["Wireframing", "Prototype Design", "User Testing", "Info Architecture"];
 var tools = ["Figma", "Adobe Illustrator"];
 var link_titles = ["Prototype"];
 var link_srcs = ["https://www.figma.com/proto/Q0qMa8yHzh4Vqpfptnxpsd/Xinyu-Wang-%26-Gaurav-Patil---Digital-Product-Design-Assignments?page-id=1%3A9&type=design&node-id=473-1178&viewport=462%2C326%2C0.07&t=glaTkkN16jSwr2rs-1&scaling=scale-down&starting-point-node-id=473%3A1178&mode=design"];
