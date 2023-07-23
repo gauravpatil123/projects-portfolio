531b7f57c8f19f235b2899981871353e73f2f75d
gauravpatil123 <gaurav.patil123@gmail.com>
2023-07-23 15:50:50 -0400

    updated problem statement text to digital product design

--- scripts/projects/digital_product_design.js
+++ scripts/projects/digital_product_design.js
@@ -1,6 +1,6 @@
 import { get_link_url } from "../utils.js";
 
-var problem_text1 = "Communicating Subsurface Infrastructure";
+var problem_text1 = 'Initial problem statement from NYC DDC: "How might we communicate subsurface infrastructure construction projects to the people."';
 var problem_text2 = "The NYC DDC is constantly building & maintaining infrastructure projects for the NYC public. But, these projects require opening up streets for an extensive amount of time.";
 var problem_text3 = "How can we better communicate the purpose & necessity of the DDC projects to NYC residents & business owners?";
 var fig1_caption = "Fig #1: <i>Search Screen in Final Prototype</i>";
