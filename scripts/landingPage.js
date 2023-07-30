e6b67227fc29ea1e9bd87e4debbb56d825fe7e61
gauravpatil123 <gaurav.patil123@gmail.com>
2023-07-30 19:16:00 -0400

    removed relative links from domain logos and used toggle classes

--- scripts/landingPage.js
+++ scripts/landingPage.js
@@ -12,19 +12,19 @@ async function click_enter(url) {
 
     e.preventDefault();
     
-    //links doent working in local environment =? its set up to work on github pages
-    setCss("#portfolio-domain-logo", "background-image", "url(/projects-portfolio/assets/logo/Portfolio-Domains-Logo-b.png)");
+    $("#domain-logo").toggleClass("portfolio-domain-logo", false)
+    $("#domain-logo").addClass("portfolio-domain-logo-clicked");
     setCss("body", "background-image", "none");
     setCss("#enter-button-link-wrapper", "display", "none");
 
     $("body").animate({ backgroundColor : "#3D6599" }, 1000);
-    $("#portfolio-domain-logo").animate({ width : "37vw", height : "37vw" }, 1000);
+    $(".portfolio-domain-logo-clicked").animate({ width : "37vw", height : "37vw" }, 1000);
 
     await new Promise(r => setTimeout(r, 2000));
     
     $("body").animate({ backgroundColor : "white" }, 1000);
     $(".logo-screen").animate({ opacity : 0 }, 1000);
-    $("#portfolio-domain-logo").animate({ opacity : 0 }, 1000);
+    $(".portfolio-domain-logo-clicked").animate({ opacity : 0 }, 1000);
 
     await new Promise(r => setTimeout(r, 1000));
     redirect(url);
