55bbebb3ec4af6cff6aa0a63132ced9c49b40d93
gauravpatil123 <gaurav.patil123@gmail.com>
2023-11-20 00:51:21 -0500

    updated interactive viz thumbnail

--- scripts/projects/interactive_viz_panel.js
+++ scripts/projects/interactive_viz_panel.js
@@ -1,6 +1,6 @@
 import {setCss, append_tag, append_link, get_link_url} from "../utils.js";
 
-var project_thumbnail_url = "../../assets/thumbnails/us-congress-fall22.png";
+var project_thumbnail_url = "../../assets/thumbnails/us-congress-fall22-6.png";
 var title = "The Transition in US Congress from 2014 - 2020";
 var skills = ["Data Visualization", "Data Collection", "Data ETL", "Prototyping"];
 var tools = ["D3", "JavaScript", "CSS", "HTML"];
