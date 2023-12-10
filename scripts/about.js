08ecbfe7ff4499f3602ad386abd3009ab1b254a9
gauravpatil123 <gaurav.patil123@gmail.com>
2023-12-10 16:16:02 -0500

    initialised new intor-detail on about page along with new script file

--- scripts/about.js
+++ scripts/about.js
@@ -1,12 +1,8 @@
-import { mobile_menu_nav } from "./utils.js";
+var intro_detail = "I am interested in digital product design, data science & web development.<br><br> I have recently graduated from Pratt Institute's School of Information with MS in Data Analytics & Visualization, and an advanced certificate in UI/UX simultaneously. Additionally, I have a strong background in engineering and applied sciences. I studied engineering in my undergrad & first grad school.<br><br> For a past half decade I have pursued data science by myself, studying the core concepts, technologies & tools. Moreover, I have a foundational grasp on machine learning and deep learning models. I have applied this knowledge in several of my personal projects.<br><br> The self study along with my formal education in data science, product designing & UI/UX have equipped me with a breadth of skillset suitable of varied job roles across all these fields.<br><br> I continuiosly keep on learning new technologies & tools in my spare time, my background makes it easier for me to grasp new technologies.<br><br> If you want to get in touch with me, take a look at my contact information below.";
 
 function main() {
 
-    if (window.matchMedia("(max-width: 1199px)").matches) {
-
-        mobile_menu_nav();
-      
-    }
+    $("#intro-detail").html(intro_detail);
 
 }
 
